51d954504ab963da2000f9f92f341b508b27aa0b
JefferGonzalez <jefferd2016@gmail.com>
2024-01-05 12:57:56 -0500

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
-
 import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeffer.dev',
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          es: 'es-CO',
+          en: 'en-US'
+        }
+      }
+    })
+  ],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
