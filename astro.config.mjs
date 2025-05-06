aa42f142d56abb1aaa441103557bd03e75222cf2
JefferGonzalez <jefferd2016@gmail.com>
2025-05-05 21:31:13 -0500

    feat: Update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jeffer.dev',
+  site: 'https://jeffer-dev.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   },
